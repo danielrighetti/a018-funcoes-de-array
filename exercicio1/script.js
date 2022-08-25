1dd2de091edd36e8344135cb5b160c2b73b766c8
Daniel Righetti <danrighetti27@gmail.com>
2022-08-25 19:03:10 -0300

    feitos

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -4,3 +4,28 @@ const objeto = {
     username: "astrodev_labenu",
     senha: "melhorDeTodos"
 }
+
+// parte a
+
+const palavrasMaiusculas = (parametro) => {
+    return {
+        nome: parametro.nome.toUpperCase(),
+        profissao: parametro.profissao.toUpperCase(),
+        username: parametro.username.toUpperCase(),
+        senha: parametro.senha.toUpperCase()
+    }
+}
+console.log(palavrasMaiusculas(objeto))
+
+
+// parte b
+
+
+
+// parte c
+
+const imprimirObjeto = (objeto, callback) => {
+    return callback(objeto)
+}
+
+console.log(imprimirObjeto(objeto,palavrasMaiusculas))
